b98a33669fd681769be0b17cf44dbd565d6d3f15
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-07-17 12:57:16 -0700

    Updated tab bar and centered dropdown

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 
+//Navbar functions 
+
 const navToggle = document.querySelector('.nav__toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 
 
-navToggle.addEventListener('mouseover', () => {
+navToggle.addEventListener('click', () => {
 	document.body.classList.toggle('nav-open');
 });
+
 
 navLinks.forEach(link => {
 	link.addEventListener('click', () => {
@@ -13,3 +16,59 @@ navLinks.forEach(link => {
 
 		})
 })
+
+//Checkbox functions
+
+// const allProjects = document.querySelectorAll('portfolio');
+
+// const pythonProjects = document.querySelectorAll('#doommapper, #placeholder1');
+
+// const javascriptProjects = document.querySelectorAll('#doommapper');
+
+$('#myList a[href="#profile"]').tab('show') // Select tab by name
+$('#myList a:first-child').tab('show') // Select first tab
+$('#myList a:last-child').tab('show') // Select last tab
+$('#myList a:nth-child(3)').tab('show') // Select third tab
+
+
+// $(document).ready(function(){
+//     $('#myList a:first-child').click(function(e){
+//         e.preventDefault();
+//         $(pythonProjects.tab('show'));
+//     });
+// });
+
+
+// function pyFunction() {
+
+// $(document).ready(function () {
+//     $('#python').change(function () {
+//         if (this.checked) 
+//         //  ^
+//            $(pythonProjects).fadeIn('slow');
+          
+//         else 
+//             $(pythonProjects).fadeOut('slow');
+            
+//     });
+// });
+
+// }
+
+
+// function jsFunction() {
+
+// $(document).ready(function () {
+//     $('#javascript').change(function () {
+//         if (this.checked) 
+//         //  ^
+//     	   $(allProjects).fadeOut('slow');
+//            $(javascriptProjects).fadeIn('slow');
+          
+//         else 
+//             $(javascriptProjects).fadeOut('slow');
+            
+//     });
+// });
+
+// }
